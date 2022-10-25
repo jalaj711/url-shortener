23494b39ec4ab06b3a8e90e59524e842c2f5b044
Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>
2022-10-25 16:38:26 +0530

    create URL model
    Signed-off-by: Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>

--- api/routes/models.py
+++ api/routes/models.py
@@ -1,3 +1,9 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+class URL(models.Model):
+    shortened = models.CharField(max_length=20)
+    actual = models.URLField()
+    date_created = models.DateTimeField(auto_now=True)
+    visits = models.IntegerField(default=0)
